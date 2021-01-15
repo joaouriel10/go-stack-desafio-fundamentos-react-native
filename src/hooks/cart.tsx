935db59af598360085ae253afcb699fc36ba821f
Joaouriel10 <joaouriel2016@outlook.com>
2021-01-14 21:59:37 -0300

    Finalizando teste app

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,39 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const product = await AsyncStorage.getItem('@products');
+
+      if (product) {
+        setProducts([...JSON.parse(product)]);
+      }
     }
 
     loadProducts();
   }, []);
 
   const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+    const productExists = products.find(p => p.id === product.id);
+
+    if (productExists) {
+      setProducts( products.map( p => p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p ))
+    } else {
+      setProducts([...products, { ...product, quantity: 1 }]);
+    }
+
+    await AsyncStorage.setItem('@products', JSON.stringify(products));
+  }, [products]);
 
   const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    setProducts(products.map(product => product.id === id ? { ...product, quantity: product.quantity + 1 } : product));
+
+    await AsyncStorage.setItem('@products', JSON.stringify(products));
+  }, [products]);
 
   const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    setProducts(products.map(product => product.id === id ? {...product, quantity: product.quantity - 1 } : product));
+
+    await AsyncStorage.setItem('@products', JSON.stringify(products));
+  }, [products]);
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
